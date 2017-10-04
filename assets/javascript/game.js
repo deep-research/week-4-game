f30d4028ba93df106b3581d1e912befc599457f2
deep-research <deep-research>
2017-10-03 23:54:11 -0500

    Improved Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,103 +8,90 @@ $(document).ready(function() {
 	// Random Function
 	function randInt(min,max) {
 	    var number = Math.floor(Math.random()*(max-min+1)+min);
-	    return number
+	    return number;
 	}
 
-	// Target Number
-	var targetNum = randInt(19, 120);
-	$("#target").html(targetNum)
-
-	// Counter Number
-	var counterNum = 0;
-	$("#counter").html(counterNum)
-
-	// Wins
+	// Variables
+	var targetNum;
+	var counterNum;
 	var winCount = 0;
-	$("#wins").html(winCount)
-
-	// Losses
+	$("#wins").html(winCount);
 	var lossCount = 0;
-	$("#losses").html(lossCount)
+	$("#losses").html(lossCount);
 
-	// Button Value
+	// Button Values
 	var btn1 = {
-		value: randInt(1, 12),
+		value: null,
 		newNum: function() {
-			this.value = randInt(1, 12)
+			this.value = randInt(1, 12);
 		}
-	}
+	};
 	var btn2 = {
-		value: randInt(1, 12),
+		value: null,
 		newNum: function() {
-			this.value = randInt(1, 12)
+			this.value = randInt(1, 12);
 		}
-	}
+	};
 	var btn3 = {
-		value: randInt(1, 12),
+		value: null,
 		newNum: function() {
-			this.value = randInt(1, 12)
+			this.value = randInt(1, 12);
 		}
-	}
+	};
 	var btn4 = {
-		value: randInt(1, 12),
+		value: null,
 		newNum: function() {
-			this.value = randInt(1, 12)
+			this.value = randInt(1, 12);
 		}
-	}
+	};
+	objList = [btn1, btn2, btn3, btn4];
 
 	// Button Click
 	$("#crystal-1").on("click", function() {
-		counterNum += btn1.value
-		$("#counter").html(counterNum)
+		counterNum += btn1.value;
+		$("#counter").html(counterNum);
 	});
 	$("#crystal-2").on("click", function() {
-		counterNum += btn2.value
-		$("#counter").html(counterNum)
+		counterNum += btn2.value;
+		$("#counter").html(counterNum);
 	});
 	$("#crystal-3").on("click", function() {
-		counterNum += btn3.value
-		$("#counter").html(counterNum)
+		counterNum += btn3.value;
+		$("#counter").html(counterNum);
 	});
 	$("#crystal-4").on("click", function() {
 		counterNum += btn4.value
 		$("#counter").html(counterNum)
 	});
 
+	var gameReset = function() {
+		counterNum = 0;
+		$("#counter").html(counterNum);
+		targetNum = randInt(19, 120);
+		$("#target").html(targetNum);
+
+		objList.forEach( function(btn) {
+			btn.newNum();
+		});
+	};
+
 	// Game Logic
+	gameReset()
 	$(".crystal-btn").on("click", function() {
-		// Winning
+		// Win
 		if (counterNum === targetNum) {
-			winCount++
-			$("#wins").html(winCount)
+			winCount++;
+			$("#wins").html(winCount);
+			gameReset();
 
-			counterNum = 0;
-			$("#counter").html(counterNum)
-			targetNum = randInt(19, 120);
-			$("#target").html(targetNum)
-
-			btn1.newNum()
-			btn2.newNum()
-			btn3.newNum()
-			btn4.newNum()
-
-			alert("You win!")
+			alert("You win!");
 		}
+		// Loose
 		else if (counterNum > targetNum) {
-			lossCount++
-			$("#losses").html(lossCount)
-
-			counterNum = 0;
-			$("#counter").html(counterNum)
-			targetNum = randInt(19, 120);
-			$("#target").html(targetNum)
-
-			btn1.newNum()
-			btn2.newNum()
-			btn3.newNum()
-			btn4.newNum()
-			
-			alert("You loose!")
-		}
+			lossCount++;
+			$("#losses").html(lossCount);
+			gameReset();
+			alert("You loose!");
+		};
 	});
 });
